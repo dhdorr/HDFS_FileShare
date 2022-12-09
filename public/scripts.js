d538d6acbfe9803c9e3570433ff8a19fe52bff82
Derek Dorr <dhdorr@gmail.com>
2022-12-08 16:24:27 -0800

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -2,6 +2,7 @@
 
 function myFun(){
   console.log("DID I MAKE IT?: ");
+  //API call to app.js to retrieve info from HDFS as JSON string
   fetch('http://localhost:8080/testme')
   .then((response) => response.json())
   .then((data) => console.log(data));
