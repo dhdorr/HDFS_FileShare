24bca15d4838d91814971798e50cd7e049f811ae
Andy Yen <andyyen42@gmail.com>
2022-12-08 17:31:25 -0800

    Created file path to text (url) conversion

--- public/scripts.js
+++ public/scripts.js
@@ -6,4 +6,22 @@ function myFun(){
   fetch('http://localhost:8080/testme')
   .then((response) => response.json())
   .then((data) => console.log(data));
+  
 }
+
+function converttoText(data) {
+    for(i = 0, i < data.length, i++) {
+        var filename = data[i].split("/").pop();
+        let url = URL.createObjectURL(data[i]);
+        download(url, filename);
+        URL.revokeObjectURL(url);
+    }
+}
+
+function download(url, filename) {
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = filename;
+  link.click();
+}
+
